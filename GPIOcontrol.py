88d73f7170577de3136056bf9bb46fc197e00c4e
wouterke1997 <65283491+wouterke1997@users.noreply.github.com>
2020-05-27 14:30:16 +0200

    Add files via upload

--- GPIOcontrol.py
+++ GPIOcontrol.py
@@ -5,12 +5,12 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import RPI.GPIO AS GPIO               # Import RPI.GPIO module
+import RPi.GPIO as GPIO               # Import RPi.GPIO module
 from time import sleep                # For a delay function sleep
 GPIO.setmode(GPIO.BCM)                # Choose BCM or BOARD
 GPIO.setup(24, GPIO.OUT)              # Set a port/pin as an output
 
-try:
+
     while True:
         GPIO.output(24,1)
         sleep(10)
